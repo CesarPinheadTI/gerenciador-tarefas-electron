aa83aea63256bb0540a977f9dd3e7a44b3875e3b
Cesar Pinhead <www.accessdenied@gmail.com>
2025-05-15 22:15:03 -0300

    aula 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
 function addTask () {
-    var input = document.getElementById("input-tarefa").value
+    var input = document.getElementById("input-tarefa");
+    var inputValue = input.value;
+    input.value = null;
+    var list = document.getElementById("taskList");
+    var li = document.createElement("li");
+    li.innerText = inputValue;
+    li.innerHTML = `${inputValue} <button class="buttondelete" onclick="this.parentElement.remove()"><img src="../../assets/delete.webp" alt="Add" width="18" height="18"> </button>`;
+    li.onclick = function() {
+        li.className = "doneTask";
+    }
+    list.appendChild(li);
 }
