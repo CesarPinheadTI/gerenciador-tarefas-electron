30ea6373cae59f08bce61deef157ec0304d6dbd8
Cesar Pinhead <www.accessdenied@gmail.com>
2025-05-16 22:29:18 -0300

    aula 3

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: "assets/add.png",
   })
 
   win.loadFile('src/pages/index.html')
--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,35 @@
+var meuInput = document.getElementById("input-tarefa");
+meuInput.addEventListener("keypress", function(event) {
+    if (event.key == "Enter") {
+        addTask();
+    }
+})
+
 function addTask () {
     var input = document.getElementById("input-tarefa");
     var inputValue = input.value;
+    if (!inputValue.trim()) {
+        alert ("Entre com uma tarefa");
+        return
+    }
     input.value = null;
     var list = document.getElementById("taskList");
     var li = document.createElement("li");
     li.innerText = inputValue;
-    li.innerHTML = `${inputValue} <button class="buttondelete" onclick="this.parentElement.remove()"><img src="../../assets/delete.webp" alt="Add" width="18" height="18"> </button>`;
+    li.innerHTML = `${inputValue} <button class="buttondelete" onclick="this.parentElement.remove()"><img src="../../assets/delete.webp" alt="Add" width="18" height="18"></button>`;
     li.onclick = function() {
         li.className = "doneTask";
     }
     list.appendChild(li);
+    showModal();
+}
+
+function showModal() {
+    var modal = document.getElementById("modal-success");
+    modal.classList.add("open-modal");
+    var audio = document.getElementById("audio-toasty");
+    audio.play();
+    setTimeout(function() {
+        modal.classList.remove('open-modal');
+    }, 400);
 }
